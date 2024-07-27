ebf1c452957e2bfa6eb029812f8059cfe8440dce
Tlezip <title0088@gmail.com>
2024-07-27 11:18:45 +0700

    add input

--- src/pages/setup/index.tsx
+++ src/pages/setup/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Button } from 'react-bootstrap'
+import { Button, Form } from 'react-bootstrap'
 import PlayerSelection from '../../components/PlayerSelection'
 import settings from '../../settings.json'
 
@@ -9,9 +9,44 @@ import './index.css'
 const Setup = () => {
     const [isSelectingAll, setIsSelectingAll] = useState<boolean>(false);
     const [selecting, setSelecting] = useState<string[]>([])
+    const [playersInput, setPlayersInput] = useState<string>('');
     const [court, setCourt] = useState<number>(0)
 
+    const getPlayersFromText = (text: string) => {
+        const playerRegex = /^\d+./
+        const players = playersInput.split('\n').filter(line => line.match(playerRegex)).map(playerText => playerText.replace(playerRegex, '').trim())
+        return players
+    }
+
     const courts = [1, 2, 3, 4]
+    const players = getPlayersFromText(playersInput)
+
+    return (
+        <div className="setup">
+            <h2>Input player to play</h2>
+            <Form>
+                <Form.Control as="textarea" onChange={e => setPlayersInput(e.target.value)} className="setup-textarea"/>
+            </Form>
+            <h2>Courts</h2>
+            <div className="courts-btn-container">
+                {
+                    courts.map(_court => (
+                        <Button
+                            className={`courts-btn ${_court === court ? 'btn-primary' : 'btn-secondary'}`}
+                            onClick={() => setCourt(_court)}
+                        >
+                            {_court}
+                        </Button>
+                    ))
+                }
+            </div>
+            <div className="button-container">
+                <Link to="/alonable" state={{ players, court }}>
+                    <Button>Submit</Button>
+                </Link>
+            </div>
+        </div>
+    )
 
     return (
         <div className="setup">
